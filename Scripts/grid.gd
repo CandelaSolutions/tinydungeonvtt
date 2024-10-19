6d3d7d9595f962c4a5b1645ca0736de67c82285c
JDHinds <jude.hinds@gmail.com>
2024-10-19 10:33:25 +0100

    Added Icons

---
+++ Scripts/grid.gd
@@ -0,0 +1,11 @@
+extends MultiMeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var meshData = []
+	meshData.resize(ArrayMesh.ARRAY_MAX)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
