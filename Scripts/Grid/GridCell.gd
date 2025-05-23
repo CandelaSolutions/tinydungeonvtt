e8deb5a1a534761574053c2a38c3c9092c343a82
JDHinds <jude.hinds@gmail.com>
2025-05-23 07:09:55 +0100

    Basic Border Generation

--- Scripts/Grid/GridCell.gd
+++ Scripts/Grid/GridCell.gd
@@ -5,25 +5,42 @@ var gridtype: int
 var closeNeighbours: Array[GridCell] # Clockwise from 12
 var farNeighbours: Array[GridCell]
 
-func _init(origin: Vector3) -> void:
+var points: Array = []
+var borderMaterial = preload("res://Visuals/Materials/border.tres")
+
+func _init(origin: Vector3, type: int, flip: bool = false) -> void:
 	transform.origin = origin
-	# Create a new MeshInstance3D
 	var mesh_instance = MeshInstance3D.new()
-	
-	# Create a CubeMesh
-	var cube_mesh = BoxMesh.new()
+	var points
 	
 	# Set the mesh for the MeshInstance3D
-	mesh_instance.mesh = cube_mesh
+	if flip:
+		points = generate_points(type, 1, PI - PI/type)
+	else:
+		points = generate_points(type, 1)
 	
-	# Optionally, set the position of the mesh instance
-	mesh_instance.position = Vector3(0, 0, 0)
+	mesh_instance.mesh = generate_cell_mesh(points)
 	mesh_instance.scale = Vector3(0.5, 0.5, 0.5)
 	
 	# Add the MeshInstance3D as a child of the current Node3D
 	add_child(mesh_instance)
+	
+	var border_mesh_instance_0 = MeshInstance3D.new()
+	border_mesh_instance_0.mesh = generate_border_mesh(points[0], points[1], 4, 0.05, PI/2)
+	border_mesh_instance_0.material_override = borderMaterial
+	add_child(border_mesh_instance_0)
+	var border_mesh_instance_1 = MeshInstance3D.new()
+	border_mesh_instance_1.mesh = generate_border_mesh(points[1], points[2], 4, 0.05, PI/2)
+	border_mesh_instance_1.material_override = borderMaterial
+	add_child(border_mesh_instance_1)
+	var border_mesh_instance_2 = MeshInstance3D.new()
+	border_mesh_instance_2.mesh = generate_border_mesh(points[2], points[0], 4, 0.05, PI/2)
+	border_mesh_instance_2.material_override = borderMaterial
+	add_child(border_mesh_instance_2)
 
-func generate_polygon(sides: int, radius: float) -> Array:
+# --------------------------------- Functions ---------------------------------
+
+func generate_points(sides: int, radius: float = 1, rotation: float = PI/sides) -> Array:
 	var points = []
 	radius = radius / cos(PI / sides)  # Calculate the outer radius
 	
@@ -31,6 +48,73 @@ func generate_polygon(sides: int, radius: float) -> Array:
 		var angle = i * 2 * PI / sides
 		var x = radius * cos(angle)
 		var y = radius * sin(angle)
-		points.append(Vector2(x, y))
+		points.append(Vector3(x, 0, y).rotated(Vector3(0, 1, 0), rotation))
 	
 	return points
+
+func generate_cell_mesh(points: Array) -> Mesh:
+	var vertices = PackedVector3Array()
+	var indices = PackedInt32Array()
+	for i in range(points.size()):
+		vertices.append(points[i])
+		if i > 1:
+			indices.append(0)
+			indices.append(i-1)
+			indices.append(i)
+
+	# Initialize the ArrayMesh.
+	var arr_mesh = ArrayMesh.new()
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+	arrays[Mesh.ARRAY_INDEX] = indices
+
+	# Create the Mesh.
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	return arr_mesh
+
+func generate_border_mesh(point1: Vector3, point2: Vector3, sides: int, radius: float, rotation: float = 0) -> Mesh:
+	var vertices = PackedVector3Array()
+	var indices = PackedInt32Array()
+	var normals = PackedVector3Array()
+
+	var points1 = []
+	var points2 = []
+	for i in range(sides):
+		var angle = i * 2 * PI / sides
+		var x = radius * cos(angle) * 1 # TODO: Thicken proportential to the bending
+		var y = radius * sin(angle)
+		var point = Vector3(x, y, 0).rotated(Vector3(0,0,1), rotation) # rotating to user's preference
+		var a = point.rotated(Vector3(0,1,0), -atan2(point1.z - y, point1.x - x)) + point1/2
+		var b = point.rotated(Vector3(0,1,0), -atan2(point2.z - y, point2.x - x)) + point2/2
+		points1.append(a)
+		points2.append(b)
+		normals.append(point.normalized())
+		normals.append(point.normalized())
+
+	for i in range(points1.size()):
+		vertices.append(points1[i])
+		vertices.append(points2[i])
+		if i > 0:
+			indices.append(2*(i-1))
+			indices.append(2*(i-1)+1)
+			indices.append(2*(i-1)+3)
+			indices.append(2*(i-1)+3)
+			indices.append(2*(i-1)+2)
+			indices.append(2*(i-1))
+	indices.append(2*points1.size()-2)
+	indices.append(2*points1.size()-1)
+	indices.append(1)
+	indices.append(1)
+	indices.append(0)
+	indices.append(2*points1.size()-2)
+
+	# Initialize the ArrayMesh.
+	var arr_mesh = ArrayMesh.new()
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+	arrays[Mesh.ARRAY_INDEX] = indices
+	arrays[Mesh.ARRAY_NORMAL] = normals
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	return arr_mesh
--- Scripts/Grid/GridLayer.gd
+++ Scripts/Grid/GridLayer.gd
@@ -9,16 +9,18 @@ func _init(gridtype: int, width: int, length: int) -> void:
 	_gridtype = gridtype
 	_width = width
 	for i in range(length * width):
-		print(i%width)
-		print(i/width)
-		# Set Location (with altering z position for hex grids)
-		var cell = GridCell.new(Vector3(i%width, 0, i/width))
-		add_child(cell)
+		var x = int(i%width)
+		var y = int(i/width)
+		# Set Location (with altering z position for hex grids
 		# Set neighbours
 		match gridtype:
+			#  This is some black magic. Do not touch.
 			3:
-				pass
+				var cell = GridCell.new(Vector3(x - 0.5*y + (x + y)/2, 0, y * 0.75 / cos(PI / 6)), gridtype, (x%2 != y%2))
+				add_child(cell)
 			4:
-				pass
+				var cell = GridCell.new(Vector3(x, 0, y), gridtype)
+				add_child(cell)
 			6:
-				pass
+				var cell = GridCell.new(Vector3(x + (y * 0.5 - y / 2), 0, y * 0.75 / cos(PI / 6)), gridtype)
+				add_child(cell)
