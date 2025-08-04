a6c0dcb899d5d583aa564895d7c620076eb88b5a
JDHinds <jude.hinds@gmail.com>
2025-08-04 22:44:41 +0100

    Added Previous Commands Windows

--- Scripts/General/Console.gd
+++ Scripts/General/Console.gd
@@ -1,8 +1,17 @@
 @tool
 extends LineEdit
 
+@onready var logsContainer = $ScrollContainer/VBoxContainer
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
 		var evLocal = make_input_local(event)
 		if !Rect2(Vector2(0,0), size).has_point(evLocal.position):
 			release_focus()
+
+func _on_text_submitted(new_text: String) -> void:
+	var label = Label.new()
+	label.text = new_text
+	logsContainer.add_child(label)
+	clear()
+	release_focus()
