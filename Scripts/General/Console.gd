ac005943070ac74fab9bfec897a088777d0eea39
JDHinds <jude.hinds@gmail.com>
2025-08-05 17:41:30 +0100

    Updated for bug where clicking previous message section would give focus back to the lineedit

--- Scripts/General/Console.gd
+++ Scripts/General/Console.gd
@@ -7,7 +7,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
 		var evLocal = make_input_local(event)
 		if !Rect2(Vector2(0,0), size).has_point(evLocal.position):
-			release_focus()
+			call_deferred("release_focus")
 
 func _on_text_submitted(new_text: String) -> void:
 	var label = Label.new()
